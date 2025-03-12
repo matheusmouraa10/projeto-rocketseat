53991f26c78a1d1282c165c1f8d7b1a378b8eb18
Matheus Moura <matheusmourasilvaa10@gmail.com>
2025-03-12 09:40:18 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+function toggleMode(){
+  const html = document.documentElement
+
+  // if(html.classList.contains('light')){
+  //  html.classList.remove('light')
+  //  } else{
+  //  html.classList.add('light')
+  //  }
+
+  html.classList.toggle('light')
+  
+          // pegar a tag img
+  //const img = document.querySelector("#profile img")
+
+         // subistituir a imagem 
+  //if(html.classList.contains('light')){
+        // se tiver light mode, adicionar a imagem light
+    //img.setAttribute('src', './acessets/avatar-light.png')
+  } 
+        // se nn estiver light mode, permanecer img normal
+  //else{
+ //   img.setAttribute('src', './acessets/avatar.png')
+ // }
